34b0eabc9831f6d0782316b61bc75e42e7a90c9f
Arpan Dhama <arpandhama2001@gmail.com>
2025-05-29 11:11:13 +0530

    MongoDB setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
 const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Connect to MongoDB
+const MONGO_URI = ServerConfig.MONGODB_URI;
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use("/api", apiRoutes);
 app.use("/options/api", apiRoutes);
